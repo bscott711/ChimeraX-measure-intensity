ec1c2dc07cd5ff4b3b83454a5a67b4085636f0f7
bscott711 <40175801+bscott711@users.noreply.github.com>
2021-10-07 11:25:45 -0600

    Initial commit

---
+++ src/cmd.py
@@ -0,0 +1,16 @@
+# vim: set expandtab shiftwidth=4 softtabstop=4:
+
+from chimerax.core.commands import CmdDesc
+
+
+def measure_intensity(session):
+    # All command functions are invoked with ``session`` as its
+    # first argument.  Useful session attributes include:
+    #   logger: chimerax.core.logger.Logger instance
+    #   models: chimerax.core.models.Models instance
+    session.logger.info("Hello world!")
+
+
+# CmdDesc contains the command description.  For the
+# "hello" command, we expect no arguments.
+measure_intensity_desc = CmdDesc()
