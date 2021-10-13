30dbe134dc8bcf342880b77d16036e1a0ae7d0d3
bscott711 <40175801+bscott711@users.noreply.github.com>
2021-10-13 10:21:31 -0600

    created measure_intensity function

--- measure_intensity.py
+++ measure_intensity.py
@@ -1,4 +1,6 @@
-def measure_distance(session, surface, to_map, radius=50, palette=None, c_range=None, key=None):
+
+def measure_intensity(session, surface, to_map, radius=9, palette=None, c_range=None, key=None):
+    from numpy import array, swapaxes
     # All command functions are invoked with ``session`` as its
     # first argument.  Useful session attributes include:
     #   logger: chimerax.core.logger.Logger instance
@@ -7,17 +9,34 @@ def measure_distance(session, surface, to_map, radius=50, palette=None, c_range=
         from chimerax.core import colors
         palette = colors.BuiltinColormaps['purples-8']
     if c_range is None:
-        c_range = (0, radius)
+        c_range = (0.75, 1.25)
     rmin, rmax = c_range
     cmap = palette.rescale_range(rmin, rmax)
 
-    _, distance = query_tree(surface.vertices, to_map.vertices, radius)
-    surface.vertex_colors = cmap.interpolated_rgba8(distance)
+    image_coords, flattened_image, pixel_indices = get_coords(to_map)
+    index, _ = query_tree(surface.vertices, image_coords.T, radius)
+    face_intensity = local_intensity(flattened_image, pixel_indices, index)
+    surface.vertex_colors = cmap.interpolated_rgba8(face_intensity)
 
     if key:
         from chimerax.color_key import show_key
         show_key(session, cmap)
-    return distance
+    return face_intensity
+
+
+def get_coords(volume):
+    """Get the coords for local intensity"""
+    from numpy import array, ravel_multi_index, swapaxes
+    level = volume.maximum_surface_level
+    image_3d = volume.full_matrix()
+    # ChimeraX uses XYZ for image, but numpy uses ZYX, swap dims
+    image_3d = swapaxes(image_3d, 0, 2)
+    image_3d *= (image_3d >= level)
+    image_coords = array(image_3d.nonzero())
+    flattened_image = image_3d.flatten()
+    pixel_indices = ravel_multi_index(image_coords, image_3d.shape)
+
+    return image_coords, flattened_image, pixel_indices
 
 
 def query_tree(init_verts, to_map, radius=50, k_nn=200):
@@ -43,18 +62,27 @@ def remove_index(index, tree_max):
     return index
 
 
+def local_intensity(flattened_image, pixel_indices, index):
+    from numpy import array, nanmean
+    """Measure local mean intensity."""
+    face_intensities = array(
+        [nanmean(flattened_image[pixel_indices[ii]]) for ii in index])
+    return face_intensities/nanmean(face_intensities)
+
+
 def register_command(session):
-    from chimerax.core.commands import CmdDesc, register, SurfaceArg, ColormapArg, ColormapRangeArg, BoolArg, FloatArg
+    from chimerax.core.commands import CmdDesc, register, SurfaceArg, ModelArg, ColormapArg, ColormapRangeArg, BoolArg, FloatArg
     desc = CmdDesc(
         required=[('surface', SurfaceArg)],
-        keyword=[('to_map', SurfaceArg),
+        keyword=[('to_map', ModelArg),
                  ('radius', FloatArg),
                  ('palette', ColormapArg),
                  ('c_range', ColormapRangeArg),
                  ('key', BoolArg)],
         required_arguments=['to_map'],
-        synopsis='measure local surface distance')
-    register('measure distance', desc, measure_distance, logger=session.logger)
+        synopsis='measure local surface intensity')
+    register('measure intensity', desc,
+             measure_intensity, logger=session.logger)
 
 
 register_command(session)
