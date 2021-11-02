fb7b0fab9b9077b76db7192d38cc08a2d9df3a76
bscott711 <40175801+bscott711@users.noreply.github.com>
2021-11-02 14:54:09 -0600

    Created helper to generate axis model

---
+++ create_axis.py
@@ -0,0 +1,39 @@
+def create_axis(coords=[430, 200, 110], dist=20, filename='axis.bild'):
+    '''Generate axes arrows in chimeraX'''
+    lines = ['.color green',
+             '.dot {0} {1} {2}'.format(*coords),
+             '.color white',
+             '.arrow {0} {1} {2} {3} {1} {2} 1 4 .6'.format(
+                 *coords, coords[0]-dist),
+             '.color green',
+             '.dot {0} {1} {2}'.format(*coords),
+             '.color red',
+             '.arrow {0} {1} {2} {0} {3} {2} 1 4 .6'.format(
+                 *coords, coords[1]+dist),
+             '.color green',
+             '.dot {0} {1} {2}'.format(*coords),
+             '.color blue',
+             '.arrow {0} {1} {2} {0} {2} {3} 1 4 .6'.format(*coords, coords[2]+dist)]
+
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line)
+            f.write('\n')
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='Create axes arrow files for ChimeraX.')
+    parser.add_argument("x", nargs='?', type=int,
+                        default=400, help="x: 400")
+    parser.add_argument("y", nargs='?', type=int,
+                        default=200, help="y: 200")
+    parser.add_argument("z", nargs='?', type=int,
+                        default=100, help="z: 100")
+    parser.add_argument("d", nargs='?', type=int,
+                        default=20, help="length: 20")
+    parser.add_argument("filename", nargs='?', type=str, default="axis.bild",
+                        help="axis.bild")
+    args = parser.parse_args()
+    create_axis([args.x, args.y, args.z], args.d, args.filename)
