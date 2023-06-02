7f47e569852ea33767ec1e5f211e3a581aee4c42
bscott711 <40175801+bscott711@users.noreply.github.com>
2023-06-01 21:19:27 -0600

    Door Tracking sql

--- generate_qr.py
+++ generate_qr.py
@@ -1,4 +1,6 @@
-"""Generates a QRcode."""
+"""Generates a QR code with a logo."""
+
+from docopt import docopt
 from qrcode import QRCode
 from qrcode.constants import ERROR_CORRECT_H
 from qrcode.image.styledpil import StyledPilImage
@@ -6,15 +8,10 @@ from qrcode.image.styles.moduledrawers import CircleModuleDrawer
 from qrcode.image.styles.colormasks import SolidFillColorMask
 
 
-def generate_qr(
-    logo_path="LabLogo.png",
-    qr_color=(38, 74, 113),
-    url="http://babylonjs-viewer.glitch.me",
-    file_name="lablogoQR.png",
-):
-    """Generate a qrcode to a new URL with a logo in the middle, and the pixels of specific color"""
+def generate_qr(logo_path, qr_color, url, file_name):
+    """Generate a QR code with a logo in the middle, and the pixels of specific color."""
     qr_code = QRCode(error_correction=ERROR_CORRECT_H)
-    # adding URL or text to QRcode
+    # adding URL or text to QR code
     qr_code.add_data(url)
     # generating QR code
     qr_code.make()
@@ -29,4 +26,23 @@ def generate_qr(
 
 
 if __name__ == "__main__":
-    generate_qr()
+    USAGE = """Usage: generate_qr.py [-h] [-c COLOR] [-l LOGO_PATH] URL FILE_NAME
+
+    Generates a QR code with a logo.
+
+    Arguments:
+    URL             The URL or text to encode in the QR code.
+    FILE_NAME       The name of the output file to save the QR code to.
+
+    Options:
+    -h --help       Show this help message and exit.
+    -c COLOR        The color of the QR code. Specify as three comma-separated values representing RGB color channels. [default: 38,74,113]
+    -l LOGO_PATH    The path to the logo image file to embed in the QR code. [default: LabLogo.png]
+    """
+    args = docopt(USAGE)
+
+    # Convert color argument from string to tuple
+    color = tuple(map(int, args['-c'].split(',')))
+
+    # Call generate_qr function with arguments
+    generate_qr(args['-l'], color, args['URL'], args['FILE_NAME'])
